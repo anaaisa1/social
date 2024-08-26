09c89c091919509d238f2da3dd95d13db4232c65
anaaisa1 <160273305+anaaisa1@users.noreply.github.com>
2024-08-26 09:40:58 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,20 @@
+export const perguntas = [
+    {
+        enunciado: "Como podemos melhorar o acesso á educação de qualidade para asa crianças de comunidades desfavorecidas?",
+        alternativas: [
+            {
+                texto: "a)Reduzir o financiamento das escolas públicas em comunidades desfavorecidas e concentrar os recursos apenas nas instituições de ensino privadas. ",
+                afirmacao:[
+                 "Errado!",
+                ""],
+                proxima: 1,
+            },
+            {
+                texto: "b)Investir na capacitação de professores e na infraestrutura das escolas em comunidades desfavorecidas, além de implementar programas de mentoria e tutoria para apoiar o desenvolvimento acadêmico das crianças. ",
+                afirmacao:[
+                 "Correto!",
+                ""]
+            }
+        ]
+    },
+];
